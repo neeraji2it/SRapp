4e0890e494c79ef523ad76671ec9cb031f78901a
rails <rails@rails>
2014-12-11 17:46:27 +0530

    Added subdomain multiple validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,9 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :subdomain, presence: true, 
+                        uniqueness: { case_sensitive: false },
+                        format: { with: /\A[\w\-]+\Z/i, message: 'contains invalid characters' },
+                        exclusion: { in: ['www'], message: 'restricted' }
 end
